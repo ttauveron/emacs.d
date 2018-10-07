69ce931bcb3c471bcc8a025ec01e20016c97d8f2
Thibaut Tauveron <thibaut.tauveron@gmail.com>
2018-10-07 12:56:09 -0400

    Add magit

--- init.el
+++ init.el
@@ -71,7 +71,10 @@
 (use-package undo-tree
   :ensure t)
 
-
+(use-package magit
+  :ensure t
+  :config
+  (global-set-key (kbd "C-x g") 'magit-status))
 
 
 
@@ -80,7 +83,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (undo-tree ace-window use-package helm doom-themes))))
+ '(package-selected-packages
+   (quote
+    (magit undo-tree ace-window use-package helm doom-themes))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
