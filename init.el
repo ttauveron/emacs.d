571e5fcb52765506df6ba7bd309336dea81d5763
Thibaut Tauveron <thibaut.tauveron@gmail.com>
2018-10-07 11:59:56 +0200

    Start over

--- init.el
+++ init.el
@@ -1,42 +1,46 @@
-; ~/.emacs
+(setq debug-on-error t)
 
+;; Minimal UI
+(scroll-bar-mode -1)
+(tool-bar-mode   -1)
+(tooltip-mode    -1)
+(menu-bar-mode   -1)
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+;; Package configs
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
+                         ("gnu"   . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
-(setq inhibit-startup-message t)
-(setq inhibit-splash-screen t)
+
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
 
 
-(load "~/.emacs.d/my-loadpackages.el")
+;; Changing windows in emacs
+(global-set-key (kbd "C-x o") 'ace-window)
 
-(load-theme 'material t)
-
-(add-hook 'after-init-hook '(lambda ()
-  (load "~/.emacs.d/my-noexternals.el")
-))
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (yasnippet magit))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Theme
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-one t))
 
 
-;; recompile all from prelude
-(defun recompile-init ()
-  "Byte-compile all your dotfiles again."
-  (interactive)
-  (byte-recompile-directory user-emacs-directory 0))
+;; Display column number
+(column-number-mode)
+;; show the other parenthesis
+(show-paren-mode)
 
-(global-set-key (kbd "C-c C-1") 'recompile-init)
+;; Highlight current line
+(global-hl-line-mode t)
+
+;; auto completion
+(ido-mode t)
+
+;;(windmove-default-keybindings)
+
