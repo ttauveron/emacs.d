c6f22c10543c4a5bc64f8401d6a319ae505c951f
Thibaut Tauveron <thibaut.tauveron@gmail.com>
2019-01-30 11:53:45 +0100

    Add telephone-line

--- init.el
+++ init.el
@@ -211,5 +211,22 @@
   )
 
 
+(use-package telephone-line
+  :ensure t
+  :config
+  (telephone-line-mode 1)
+  (setq telephone-line-lhs
+      '((evil   . (telephone-line-evil-tag-segment))
+        (accent . (telephone-line-vc-segment
+                   telephone-line-erc-modified-channels-segment
+                   telephone-line-process-segment))
+        (nil    . (telephone-line-minor-mode-segment
+                   telephone-line-buffer-segment))))
+  (setq telephone-line-rhs
+      '((nil    . (telephone-line-misc-info-segment))
+        (accent . (telephone-line-major-mode-segment))
+        (evil   . (telephone-line-airline-position-segment))))
+)
+
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
